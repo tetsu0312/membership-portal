b61462ff34dc7a87acbe8135f94167a4924bff0e
tetsuyashibata0312 <march312march@gmail.com>
2026-01-11 19:10:39 +0900

    Update register.js

--- js/register.js
+++ js/register.js
@@ -2,14 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔽 Firebase設定（コンソールからコピペ）
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCQKXq7z-tgfz5H38G-GLpZWsEG3MA8i2Q",
+  authDomain: "user-login-portal.firebaseapp.com",
+  projectId: "user-login-portal",
+  storageBucket: "user-login-portal.firebasestorage.app",
+  messagingSenderId: "183527442150",
+  appId: "1:183527442150:web:15341cf06a216c10ab9d27",
+  measurementId: "G-NG9H5NJEPH"
 };
 
 const app = initializeApp(firebaseConfig);
