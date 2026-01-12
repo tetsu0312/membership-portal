fa989f7c19b3100577da1b0071fa53adb560c297
tetsu0312 <tetsu0312@gmail.com>
2026-01-12 21:12:30 +0900

    1.5秒遅延でchatplus読み込み

--- js/chatplus.js
+++ js/chatplus.js
@@ -1,36 +1,26 @@
-(function () {
+setTimeout(function () {
+  (function(){
+    var w = window, d = document;
+    var s = "https://app.chatplus.jp/cp.js";
 
-  function waitForSessionData(callback) {
-    const timer = setInterval(() => {
-      // 必須キーが揃ったらOK
-      if (
-        sessionStorage.getItem("memberNo") &&
-        sessionStorage.getItem("chatName")
-      ) {
-        clearInterval(timer);
-        callback();
-      }
-    }, 100); // 100msごとにチェック
-  }
+    d["__cp_d"] = "https://app.chatplus.jp";
+    d["__cp_c"] = "c0632e38_92";
 
-(function(){
-var w=window,d=document;
-var s="https://app.chatplus.jp/cp.js";
-d["__cp_d"]="https://app.chatplus.jp";
-d["__cp_c"]="c0632e38_92";
-  // ✅ sessionStorage から直接読む
-  d["__cp_p"] = {
-    "chatName": sessionStorage.getItem("chatName") || "",
-    "chatEmail": sessionStorage.getItem("chatEmail") || "",
-  };
-  d["__cp_f"] = {
-    "会員番号": sessionStorage.getItem("memberNo") || "",
-    "誕生日": sessionStorage.getItem("birthday") || "",
-  };
-var a=d.createElement("script"), m=d.getElementsByTagName("script")[0];
-a.async=true,a.src=s,m.parentNode.insertBefore(a,m);})();
+    // sessionStorage から読む
+    d["__cp_p"] = {
+      chatName: sessionStorage.getItem("chatName") || "",
+      chatEmail: sessionStorage.getItem("chatEmail") || ""
+    };
 
-  // 🔑 ここがポイント
-  waitForSessionData(initChatPlus);
+    d["__cp_f"] = {
+      "会員番号": sessionStorage.getItem("memberNo") || "",
+      "誕生日": sessionStorage.getItem("birthday") || ""
+    };
 
-})();
+    var a = d.createElement("script");
+    var m = d.getElementsByTagName("script")[0];
+    a.async = true;
+    a.src = s;
+    m.parentNode.insertBefore(a, m);
+  })();
+}, 1500); // ← 1500ms = 1.5秒
